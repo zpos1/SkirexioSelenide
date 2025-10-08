c33b8b6be34674b0b15eea83a8742f1594aa7750
Александр <zpos@yandex.ru>
2025-10-08 17:46:13 +0300

    add test chooseAsterUSDT

--- src/test/java/pages/SmartTerminalPage.java
+++ src/test/java/pages/SmartTerminalPage.java
@@ -1,15 +1,37 @@
 package pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import org.openqa.selenium.By;
+import io.qameta.allure.Step;
+
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class SmartTerminalPage {
     public final SelenideElement smartTerminalPage = $x("//*[contains(text(), 'Биржевой аккаунт')]");
+    public final SelenideElement tradingPair = $("[class='tw-text-[#2ECD99]']");
+    public final SelenideElement chosePair = $("[placeholder='Выберите торговую пару']");
 
-    public void SmartTerminalPageExist() {
+    public SmartTerminalPage SmartTerminalPageExist() {
         smartTerminalPage.shouldBe(visible);
+        return this;
+    }
+
+    @Step("Открытие списка Trading Pair")
+    public SmartTerminalPage pushTradingPair() {
+        tradingPair.click();
+        return this;
+    }
+
+    @Step("Выбор пары AsterUSDT")
+    public SmartTerminalPage chooseAsterUSDT() {
+        chosePair.val("aster");
+        return this;
+    }
+
+    public SmartTerminalPage waitPageLoading() {
+        smartTerminalPage.shouldBe(Condition.visible);
+        return this;
     }
 }
--- src/test/java/tests/TerminalTest.java
+++ src/test/java/tests/TerminalTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.qameta.allure.Step;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
@@ -10,7 +11,7 @@ public class TerminalTest extends BaseTest {
     private static final Logger log = LoggerFactory.getLogger(TerminalTest.class);
 
     @Test(description = "Переход на страницу Терминала")
-    public void projectLogin() {
+    public void smartTerminalOpen() {
         loginPage.openPage(url)
                 .login(UserFactory.withAdminPermision())
                 .pressSubmit();
@@ -18,5 +19,13 @@ public class TerminalTest extends BaseTest {
                 .pressTerminalButton()
                 .pressSmartTerminalButton();
         smartTerminalPage.SmartTerminalPageExist();
+    }
+
+    @Test(description = "Открытие смарт-терминала, выбор торговой пары AsterUSDT")
+    public void chooseAsterUSDT() {
+        smartTerminalOpen();
+        smartTerminalPage.waitPageLoading()
+                .pushTradingPair()
+                .chooseAsterUSDT();
     }
 }
